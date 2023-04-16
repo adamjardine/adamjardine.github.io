330633122fc2fbb3561418ba608025a4a36622f5
Adam <adam.jardine@rutgers.edu>
2023-04-16 15:51:18 +0900

    strike over!

---
+++ flt/auto/references.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "buchi60"
+    "engelfriethoogeboom01"
+    "heinz10"
+    "rogerspullum11"
+    "mcnaughtonpapert71"
+    "cook78"
+    "filiot15"
+    "immerman80"))
+ :bibtex)
+
